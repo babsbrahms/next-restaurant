1dda136e6a986592455dc552529756f55a099b2d
olayinka <babsvanbrahms@gmail.com>
2021-05-09 17:44:15 +0100

    graphql setup done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from "@apollo/client/react"
+import '../styles/globals.css';
+
+// https://graphql.fauna.com/graphql
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,86 @@
 import React from "react"
+import axios from "axios"
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { ApolloConsumer } from "@apollo/client/react"
 
-export default function Home() {
+
+let EXCHANGE_RATE = gql`
+  query {
+    rates(currency: "USD") {
+      rate
+      name
+      currency
+    }
+  }
+`
+function Home({ restaurants }) {
+  console.log(restaurants)
+  if (!restaurants) {
+    return <h3>Loading</h3>
+    
+  }
   return (
     <div>
       Home
+      <ul>
+        {restaurants && restaurants.map(({ name, description, logo, _id }) => (
+          <li key={_id}>
+            <h1>{name}</h1>
+            <em>{email}</em>
+            <p>{address}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
+
+
+export async function getStaticProps () {
+  // const { data, error, loading } = useQuery(EXCHANGE_RATE)
+
+  let { data: { data , errors } } =  await axios({
+    url: "https://graphql.fauna.com/graphql",
+    method: "POST",
+    headers: {
+       "Authorization": `Bearer ${process.env.FAUNA_KEY}`
+    },
+    data: {
+       query: `
+       query {
+        restaurants {
+         data {
+           name
+           description
+           logo
+           _id
+         }
+       }
+     }
+     `,
+       variables: {
+         
+       }
+     }
+  })
+
+  let restaurants = data.restaurants.data
+
+  if (errors) {
+   return {
+      notFound: true, // 404
+      // redirect: {
+      //   destination: '/',
+      //   permanent: false,
+      // },
+   }
+  }
+  return {
+    props: {
+      restaurants
+    }
+  }
+}
+
+
+export default Home;
